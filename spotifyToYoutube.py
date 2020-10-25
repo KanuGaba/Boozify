7a5067cb10ed9a0ec430cbdfbf6193e2bb7545ec
Kanu Gaba <kgaba@umich.edu>
2020-10-25 18:37:10 -0400

    Fix errors

--- spotifyToYoutube.py
+++ spotifyToYoutube.py
@@ -18,21 +18,21 @@ def getResultsFromSeed(seed, seedType):
     if seedType == "genre":
         # may need to make this a dropdown and use recommendation_genre_seeds()
         # just gonna assume for now that they know what they're doing
-        return recommendations(seed_genre=seed, limit=60)
+        return spotify.recommendations(seed_genre=seed, limit=60)
     elif seedType == "artist":
         results = spotify.search(q='artist:' + seed, type='artist')
         items = results['artists']['items']
         if len(items) > 0:
             artist = items[0]
             # need to check that artist['url'] works
-            return recommendations(seed_artists=[artist['external_urls']['spotify']], limit=60)
+            return spotify.recommendations(seed_artists=[artist['external_urls']['spotify']], limit=60)
     elif seedType == "song":
         results = spotify.search(q='name:' + seed, type='track')
         # have to double check results dict
         items = results['tracks']['items']
         if len(items) > 0:
             track = items[0]
-            return recommendations(seed_tracks=[track['url']], limit=60)
+            return spotify.recommendations(seed_tracks=[track['url']], limit=60)
 
 def getResultsFromPlaylist(playlistURL):
     # Creating and authenticating our Spotify app.
@@ -78,8 +78,8 @@ if (__name__ == "__main__"):
     if playlistURL != "":
         tracks = getTracks(getResultsFromPlaylist(playlistURL))
     else:
-        seed = str(input("Select \"genre\", \"artist\", or \"song\": "))
-        seedType = str(input("Insert genre, artist, or song: "))
+        seedType = str(input("Select \"genre\", \"artist\", or \"song\": "))
+        seed = str(input("Insert genre, artist, or song: "))
         tracks = getTracks(getResultsFromSeed(seed, seedType))
 
     print("Searching songs...")
