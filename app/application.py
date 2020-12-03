a214782bc9bb21300f550c27350ae434c4fa2083
Kanu Gaba <kgaba@umich.edu>
2020-12-02 22:23:33 -0500

    Add loading bar, playlist shuffle, remove duplicates, limplaylist size, increase playlist size using seed, multiple attempt recommendation search

--- app/application.py
+++ app/application.py
@@ -1,89 +1,137 @@
 #coding: utf-8
 import json
 import flask
-# Spotify library.
+import random
+# Spotify library
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-# Youtube stuff.
+# Youtube stuff
 import youtube
 from youtube_search import YoutubeSearch
 
+# Constant vars
+NUM_SONGS = 60
+NUM_ATTEMPTS = 3
+
 application = flask.Flask(__name__)
 
-# Opening our JSON configuration file (which has our tokens).
+# Opening our JSON configuration file (which has our tokens)
 with open("config.json", encoding='utf-8-sig') as json_file:
     APIs = json.load(json_file)
 
 def getTracksFromSeed(seed, seedType):
-    # Creating and authenticating our Spotify applicationlication.
+    # Creating and authenticating our Spotify applicationlication
     client_credentials_manager = SpotifyClientCredentials(APIs["spotify"]["client_id"], APIs["spotify"]["client_secret"])
     spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    if seedType == "genre":
-        if seed in spotify.recommendation_genre_seeds()['genres']:
-            results = spotify.recommendations(seed_genres=[seed], limit=10)
-    elif seedType == "artist":
-        search = spotify.search(q='artist:' + seed, type='artist')
-        items = search['artists']['items']
-        if len(items) > 0:
-            artist = items[0]
-            results = spotify.recommendations(seed_artists=[artist['external_urls']['spotify']], limit=10)
-    # elif seedType == "song":
-    #     results = spotify.search(q='name:' + seed, type='track')
-    #     items = results['tracks']['items']
-    #     if len(items) > 0:
-    #         track = items[0]
-    #         return spotify.recommendations(seed_tracks=[track['url']], limit=60)
-    trackList = []
-    # For each track in the playlist.
-    for i in results["tracks"]:
-        # In case there's only one artist.
-        if (i["artists"].__len__() == 1):
-            # We add trackName - artist.
-            trackList.append(i["name"] + " - " + i["artists"][0]["name"])
-        # In case there's more than one artist.
-        else:
-            nameString = ""
-            # For each artist in the track.
-            for index, b in enumerate(i["artists"]):
-                nameString += (b["name"])
-                # If it isn't the last artist.
-                if (i["artists"].__len__() - 1 != index):
-                    nameString += ", "
-            # Adding the track to the list.
-            trackList.append(i["name"] + " - " + nameString)
+    trackList = set()
+    attempts = NUM_ATTEMPTS
+    while attempts != 0:
+        song_limit = NUM_SONGS - len(trackList)
+
+        # Exit loop if we received NUM_SONGS in the trackList
+        if song_limit == 0:
+            break
+
+        print("Attempt Left {}".format(attempts - 1))
+
+        # Spotify API to find search results
+        if seedType == "genre":
+            if seed in spotify.recommendation_genre_seeds()['genres']:
+                results = spotify.recommendations(seed_genres=[seed], limit=song_limit)
+        elif seedType == "artist":
+            search = spotify.search(q='artist:' + seed, type='artist')
+            items = search['artists']['items']
+            if len(items) > 0:
+                artist = items[0]
+                results = spotify.recommendations(seed_artists=[artist['external_urls']['spotify']], limit=song_limit)
+
+        for i in results["tracks"]:
+            # If there's only one artist add "trackName - artist"
+            if (i["artists"].__len__() == 1):
+                trackList.add(i["name"] + " - " + i["artists"][0]["name"])
+            # Else add "trackName - artist1, artist2, etc"
+            else:
+                nameString = ""
+                for index, b in enumerate(i["artists"]):
+                    nameString += (b["name"])
+                    if (i["artists"].__len__() - 1 != index):
+                        nameString += ", "
+                trackList.add(i["name"] + " - " + nameString)
+
+        attempts -= 1
 
     return trackList
 
 def getTracksFromPlaylist(playlistURL):
-    # Creating and authenticating our Spotify application.
+    # Creating and authenticating our Spotify application
     client_credentials_manager = SpotifyClientCredentials(APIs["spotify"]["client_id"], APIs["spotify"]["client_secret"])
     spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    # Getting a playlist.
+    # Spotify API to get a playlist
     results = spotify.user_playlist_tracks(user="",playlist_id=playlistURL)
 
-    trackList = []
-    # For each track in the playlist.
+    trackList = set()
+
     for i in results["items"]:
-        # In case there's only one artist.
+        # Limit playlist to NUM_SONGS
+        if len(trackList) == NUM_SONGS:
+            break
+
+        # If there's only one artist add "trackName - artist"
         if (i["track"]["artists"].__len__() == 1):
-            # We add trackName - artist.
-            trackList.append(i["track"]["name"] + " - " + i["track"]["artists"][0]["name"])
-        # In case there's more than one artist.
+            trackList.add(i["track"]["name"] + " - " + i["track"]["artists"][0]["name"])
+        # Else add "trackName - artist1, artist2, etc"
         else:
             nameString = ""
-            # For each artist in the track.
             for index, b in enumerate(i["track"]["artists"]):
                 nameString += (b["name"])
-                # If it isn't the last artist.
                 if (i["track"]["artists"].__len__() - 1 != index):
                     nameString += ", "
-            # Adding the track to the list.
-            trackList.append(i["track"]["name"] + " - " + nameString)
+            trackList.add(i["track"]["name"] + " - " + nameString)
+
+    # If playlist is smaller than NUM_SONGS use first artist of first track and search using seed
+    if len(trackList) != NUM_SONGS:
+        # Get first artist
+        track_info = next(iter(trackList))
+        end_index = track_info.find(",")
+        if end_index == -1:
+            end_index = len(track_info)
+        artist = track_info[track_info.find("-") + 2 : end_index]
+
+        attempts = NUM_ATTEMPTS
+        while attempts != 0:
+            song_limit = NUM_SONGS - len(trackList)
+
+            # Exit loop if we received NUM_SONGS in the trackList
+            if song_limit == 0:
+                break
+
+            print("Attempt Left {}".format(attempts - 1))
+
+            # Spotify API to find search results
+            search = spotify.search(q='artist:' + artist, type='artist')
+            items = search['artists']['items']
+            if len(items) > 0:
+                artist = items[0]
+                results = spotify.recommendations(seed_artists=[artist['external_urls']['spotify']], limit=song_limit)
+
+            for i in results["tracks"]:
+                # If there's only one artist add "trackName - artist"
+                if (i["artists"].__len__() == 1):
+                    trackList.add(i["name"] + " - " + i["artists"][0]["name"])
+                # Else add "trackName - artist1, artist2, etc"
+                else:
+                    nameString = ""
+                    for index, b in enumerate(i["artists"]):
+                        nameString += (b["name"])
+                        if (i["artists"].__len__() - 1 != index):
+                            nameString += ", "
+                    trackList.add(i["name"] + " - " + nameString)
 
     return trackList
 
+# Comments use Youtube API rather than web scraping
 def searchYoutube(songName):
     songName += " Music Video"
     #api = youtube.API(client_id=APIs["youtube"]["client_id"],
@@ -94,38 +142,53 @@ def searchYoutube(songName):
     #return video["items"][0]["id"]["videoId"]
     return video[0]["id"]
 
-@application.route('/get-link', methods=['POST', 'GET'])
-def getLink():
-    context = {}
-    songs = []
-    link = ""
+@application.route('/get-tracks', methods=['POST', 'GET'])
+def getTracks():
+    # songs = []
+    tracks = set()
+
     if flask.request.method == 'POST':
+        # Get user input and seach using Spotify API
         data = flask.request.get_json()
         if data['seed'] == "Artist":
-            if data['search'] == "Beyonce":
-                songs = ["Ob7vObnFUJc", "sQgd6MccwZc", "a9HIaGcBocc"]
-            else:
-                artist = data['search']
-                tracks = getTracksFromSeed(artist, "artist")
-                for i in tracks:
-                    songs.append(searchYoutube(i))
+            artist = data['search']
+            tracks = getTracksFromSeed(artist, "artist")
         elif data['seed'] == "Genre":
             genre = data['search']
             tracks = getTracksFromSeed(genre, "genre")
-            for i in tracks:
-                songs.append(searchYoutube(i))
         elif data['seed'] == "Playlist":
             playlistURL = data['search']
             tracks = getTracksFromPlaylist(playlistURL)
-            for i in tracks:
-                songs.append(searchYoutube(i))
-            
-    return flask.make_response(flask.jsonify({"ids": songs}))
+        
+        # Find YouTube video id from track
+        # for track in tracks:
+        #     songs.append(searchYoutube(track))      
+    
+    # Convert set to list and randomize
+    trackList = list(tracks)
+    random.shuffle(trackList)
+    print(trackList)
+
+    return flask.make_response(flask.jsonify({"tracks": trackList}))
+
+@application.route('/get-video-id', methods=['POST', 'GET'])
+def getVideoID():
+    video_id = ""
+
+    if flask.request.method == 'POST':
+        data = flask.request.get_json()
+        video_id = searchYoutube(data['track'])
+
+    return flask.make_response(flask.jsonify({"video_id": video_id}))
 
 @application.route('/', methods=['POST', 'GET'])
 def index():
+    # token = spotipy.util.prompt_for_user_token(APIs["spotify"]["username"], "playlist-modify-private",
+    #                                        client_id=APIs["spotify"]["client_id"],
+    #                                        client_secret=APIs["spotify"]["client_secret"],
+    #                                        redirect_uri='http://localhost:8080/')
+    # spotify_client = spotipy.Spotify(auth=token)
     return flask.render_template('web.html')
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8080,debug=True)
-    
+    application.run(host='0.0.0.0', port=8080, debug=True)
---
+++ app/static/js/main.js
@@ -0,0 +1,158 @@
+// GLOBAL VARS
+var seedString;
+var player;
+var start = 0;
+var end = start + 60;
+
+var id_list = new Array();
+var song_num = 0;
+var event_trigger = 1;
+var first_played = false;
+var lastSong;
+
+function onYouTubeIframeAPIReady() {
+    console.log("youtube api installed");
+}
+
+function onPlayerStateChange(event) {
+    if (event.data != YT.PlayerState.PLAYING && first_played){
+        console.log("beer image");
+    }
+    console.log("player state: ", event.data);
+    if (event.data == YT.PlayerState.PLAYING && first_played == false){
+        first_played = true;
+    }
+
+    if (event.data == YT.PlayerState.ENDED){
+        if (event_trigger == 0){
+            event_trigger = 1;
+            console.log("double fire");
+            return;
+        }
+        else {
+            console.log(id_list.length, " big boy");
+            console.log("first fire");
+            event_trigger = 0;
+        }
+        //if (player.videoId == lastSong){
+        //console.log("we outtie");
+        //console.log(lastSong);
+        //player.destroy();
+        //}
+        if (song_num >= id_list.length) {
+        //  console.log("end player");
+            //console.log(song_num);
+            song_num = 0;
+            id_list = new Array();
+            player.destroy();
+        }
+        //loads and plays next video
+        //player.loadVideoById(id);
+        console.log("player ended");
+        player.loadVideoById({videoId:id_list[song_num], endSeconds:60});
+        song_num += 1;
+        console.log(song_num);
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+function SelectQueryType(clicked_id){
+    document.getElementsByClassName("chooseOne")[0].innerHTML = clicked_id;
+    document.getElementsByClassName("searchQuery")[0].disabled = false;
+    seedString = clicked_id;
+}
+
+function SearchInput(){
+    document.getElementById("power_hour").disabled = false;
+}
+
+function MakePowerHour() {
+    fetch("/get-tracks", {
+        method: "POST",
+        headers: new Headers({
+            "content-type": "application/json"
+        }),
+        body: JSON.stringify({
+            "seed": seedString,
+            "search": document.getElementById('search').value,
+        })
+    })
+    .then(function(response) {
+        console.log(response);
+        response.json().then( data => {//function(data) {
+            console.log(data);
+
+            document.getElementById("loading_label").removeAttribute("hidden");
+            document.getElementById("loading").removeAttribute("hidden");
+
+            let promises = []
+            for (track of data.tracks) {
+                var trackCount = 1;
+                promises.push(
+                    fetch("/get-video-id", {
+                        method: "POST",
+                        headers: new Headers({
+                            "content-type": "application/json"
+                        }),
+                        body: JSON.stringify({
+                            "track": track,
+                        })
+                    })
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        console.log(data.video_id);
+                        id_list.push(data.video_id);
+                        document.getElementById("loading").setAttribute("value", trackCount);
+                        document.getElementById("loading").innerHTML = trackCount + "%";
+                        ++trackCount;
+                        return Promise.resolve();
+                    })
+                    .catch(console.error)
+                );
+            }
+            Promise.all(promises).then(function() {
+                console.log(id_list);
+
+                document.getElementById("loading_label").setAttribute("hidden", true);
+                document.getElementById("loading").setAttribute("hidden", true);            
+        
+                if (id_list.length > 0) {
+                    lastSong = id_list[id_list.length - 1];
+                    
+                    player = new YT.Player('player', {
+                        height: '100%',
+                        width: '100%',
+                        videoId: id_list[0],
+                        // videoId: 'M7lc1UVf-VE',
+                        playerVars: {
+                            start: start,
+                            end: end,
+                            controls: 0,
+                            disablekb: 1,
+                            fs: 1
+                        },
+                        events: {
+                            //'onReady': onPlayerReady,
+                            'onStateChange': onPlayerStateChange
+                        }
+                    });
+                    song_num += 1;
+                }
+            })
+        });
+    })
+    .catch(console.error);
+    //if id_list is not empty then load the youtube player
+}
+
+function DestroyPowerHour() {
+   // document.getElementById("deactivate").disabled = true;
+    player.destroy();
+    id_list = new Array();
+    song_num = 0;
+}
